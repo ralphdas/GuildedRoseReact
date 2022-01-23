33a7817da5b1be6d9b85623a777c81249337384f
Ralph Das <ralph.das@gmail.com>
2022-01-23 11:52:24 +0100

    feat(Gildenrose): first hook based solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { ButtonComponent } from "./components/ButtonComponent";
+import ItemListComponent from "./components/ItemListComponent";
+import initialState from "./state";
+
+// HOOK
+function useUpdateQuality() {
+  const [value, setValue] = useState(initialState.items);
+  return () =>
+    setValue((value) => {
+      return initialState.updateQuality();
+    });
+}
 
 function App() {
+  const updateQuality = useUpdateQuality();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gildenrose__container">
+      <ButtonComponent onClick={updateQuality} className="gildenrose__button">
+        Update Quality
+      </ButtonComponent>
+      <ItemListComponent items={initialState.items}></ItemListComponent>
     </div>
   );
 }
